cb409c3039ee7f37e5de5ec1c22c5837f1d8a76a
esyed3426 <esyed3426@gmail.com>
2022-07-05 21:44:50 -0400

    passed in props from top-level, added theme constants

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import {useEffect} from 'react';
+import WebFont from 'webfontloader';
 import { CssBaseline} from '@mui/material';
 import { ESNavBar } from './components/ESNavBar';
 import { ESDynamicGreeting } from './components/ESDynamicGreeting';
 
-const App = () => {
 
+const App = () => {
+  useEffect(() => {
+    WebFont.load({
+        google: {
+            families: ['Montserrat', 'Times New Roman', 'Finlandica', 'Bubblegum Sans']
+        }
+    })
+  }, []);
+  const themeColor = '#902bf5';
+  const themeFont = 'Bubblegum Sans';
+  const themeFontWeight = 'normal';
+  
   return (
     <>
       <CssBaseline />
-      <ESNavBar />
-      <ESDynamicGreeting />
+      <ESNavBar themeColor={themeColor} themeFont={themeFont} themeFontWeight={themeFontWeight}/>
+      <ESDynamicGreeting themeColor={themeColor} themeFont={themeFont} themeFontWeight={themeFontWeight}/>
+      <div style={{marginBottom: '1000px'}}></div>
     </>
   );
 }
