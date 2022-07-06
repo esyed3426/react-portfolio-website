19e8c0deb42fb003d9a52e86c863bdadc2352609
esyed3426 <esyed3426@gmail.com>
2022-07-05 20:20:55 -0400

    NavBar: Updated it to rerender dynamically

--- frontend/src/components/ESNavBar.js
+++ frontend/src/components/ESNavBar.js
@@ -1,39 +1,51 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import {Button, AppBar, Toolbar, Box} from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import dancing from './media/stickfigureDancing.gif';
 
-const themeColor = '#5D3FD3';
-const themeFont = 'Montserrat';
-const themeFontWeight = 'bold'
 
-export const ESNavBar = () => {
+export const ESNavBar = (props) => {
+
+    const { themeColor, themeFont, themeFontWeight } = props;
+    const [themeColorState, setThemeColorState] = useState(themeColor);
+    const [themeFontState, setThemeFontState] = useState(themeFont);
+    const [themeFontWeightState, setThemeFontWeightState] = useState(themeFontWeight);
+
+    useEffect(() => {
+        setThemeColorState(themeColor);
+        setThemeFontState(themeFont);
+        setThemeFontWeightState(themeFontWeight);
+        console.log('mounting ESDynamicGreeting...');
+        return () => {console.log('unmounting ESDynamicGreeting...')}
+    }
+    , [themeColor, themeFont, themeFontWeight]);
+
     return (
-        <AppBar id='appBar' position='sticky' sx={{bgcolor: 'transparent'}}>
+        <AppBar id='appBar' position='static' sx={{bgcolor: 'transparent'}}>
           <Toolbar>
             <Box display='flex' flexGrow={1}>
               <Button sx={{border: '0'}} >
-                <HomeIcon fontSize='large' sx={{color: themeColor, flex:'1'}} ></HomeIcon>
+                <HomeIcon fontSize='large' sx={{color: themeColorState, flex:'1'}} ></HomeIcon>
               </Button>
-              <Button fontSize='20px' sx={{marginLeft: '30px', color: themeColor, fontFamily: themeFont, fontWeight: themeFontWeight}} onClick={() => alert("Why did you feel the need to click on me? So intrusive...")}>
+              <Button style={{fontSize:'25px'}} sx={{marginLeft: '30px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState}} onClick={() => alert("Why did you feel the need to click on me? So intrusive...")}>
                 Ehsan Syed
               </Button>
             </Box>
             <Box>
-              <Button sx={{border: '0', fontFamily: themeFont, fontWeight: themeFontWeight}} disabled>
+              <Button sx={{border: '0', fontFamily: themeFontState, fontWeight: themeFontWeightState}} disabled>
                 <img
                   width="20px"
                   src={dancing}
                   alt='stick figure dancing'
                 />
               </Button>
-              <Button fontSize='20px' sx={{margin: '0px 10px', color: themeColor, fontFamily: themeFont, fontWeight: themeFontWeight}} onClick={() => alert("You are navigating to the about me page.")} > 
+              <Button style={{fontSize:'25px'}} sx={{margin: '0px 10px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState}} onClick={() => alert("You are navigating to the about me page.")} > 
                 About Me
               </Button>
-              <Button fontSize='20px' sx={{margin: '0px 10px', color: themeColor, fontFamily: themeFont, fontWeight: themeFontWeight}} onClick={() => alert("You are going to look at some of my projects.")} > 
+              <Button style={{fontSize:'25px'}} sx={{margin: '0px 10px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState}} onClick={() => alert("You are going to look at some of my projects.")} > 
                 My Projects
               </Button>
-              <Button fontSize='20px' sx={{margin: '0px 10px', color: themeColor, fontFamily: themeFont, fontWeight: themeFontWeight}} onClick={() => alert("You are going to take a look at my resume.")} > 
+              <Button style={{fontSize:'25px'}} sx={{margin: '0px 10px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState}} onClick={() => alert("You are going to take a look at my resume.")} > 
                 Resume
               </Button>
             </Box>
