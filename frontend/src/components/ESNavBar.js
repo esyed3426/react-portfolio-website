f2017b03874e6e9dd50b9d713dfb8658590f5a3a
esyed3426 <esyed3426@gmail.com>
2022-07-05 21:43:25 -0400

    small changes to the navbar

--- frontend/src/components/ESNavBar.js
+++ frontend/src/components/ESNavBar.js
@@ -20,24 +20,24 @@ export const ESNavBar = (props) => {
     , [themeColor, themeFont, themeFontWeight]);
 
     return (
-        <AppBar id='appBar' position='static' sx={{bgcolor: 'transparent'}}>
+        <AppBar id='appBar' position='static' sx={{bgcolor: themeColorState}}>
           <Toolbar>
             <Box display='flex' flexGrow={1}>
               <Button sx={{border: '0'}} >
-                <HomeIcon fontSize='large' sx={{color: themeColorState, flex:'1'}} ></HomeIcon>
+                <HomeIcon fontSize='large' sx={{color: 'white', flex:'1'}} ></HomeIcon>
               </Button>
-              <Button sx={{marginLeft: '30px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}} onClick={() => alert("Why did you feel the need to click on me? So intrusive...")}>
+              <Button sx={{marginLeft: '30px', color: 'white', fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}} onClick={() => alert("Why did you feel the need to click on me? So intrusive...")}>
                 Ehsan Syed
               </Button>
             </Box>
             <Box>
-              <Button sx={{margin: '0px 10px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}} onClick={() => alert("You are navigating to the about me page.")} > 
+              <Button sx={{margin: '0px 10px', color: 'white', fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}} onClick={() => alert("You are navigating to the about me page.")} > 
                 About Me
               </Button>
-              <Button sx={{margin: '0px 10px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}}  onClick={() => alert("You are going to look at some of my projects.")} > 
+              <Button sx={{margin: '0px 10px', color: 'white', fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}}  onClick={() => alert("You are going to look at some of my projects.")} > 
                 My Projects
               </Button>
-              <Button sx={{margin: '0px 10px', color: themeColorState, fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}}  onClick={() => alert("You are going to take a look at my resume.")} > 
+              <Button sx={{margin: '0px 10px', color: 'white', fontFamily: themeFontState, fontWeight: themeFontWeightState, fontSize:'25px'}}  onClick={() => alert("You are going to take a look at my resume.")} > 
                 Resume
               </Button>
             </Box>
